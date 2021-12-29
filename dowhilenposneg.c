5baea7e1d1a51b6ec2a4894752f79f7aee7e2a43
Paardha-Tejaswi-Saladi <95211056+Paardha-Tejaswi-Saladi@users.noreply.github.com>
2021-12-29 14:39:12 +0530

    Add files via upload

---
+++ dowhilenposneg.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+main()
+{
+	int i=1,n;
+	printf("Enter the value of n",n);
+	scanf("%d",&n);
+	do
+	{
+		printf("Enter the value of n",n);
+		scanf("%d",&n);
+	}
+	while(0<=n);
+}
