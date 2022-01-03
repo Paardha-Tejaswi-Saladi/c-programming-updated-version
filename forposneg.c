91cb325b7a832461e8cc01814cecac9b8b8c438f
Paardha-Tejaswi-Saladi <95211056+Paardha-Tejaswi-Saladi@users.noreply.github.com>
2022-01-03 14:41:38 +0530

    Add files via upload

---
+++ forposneg.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+main()
+{
+	int n;
+	for(n=0;n>=0;)
+	{
+		printf("Enter the value of n");
+	    scanf("%d",&n);
+		printf("%d\n",n);
+	}
+}
---
+++ forsum.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+main()
+{
+	int n,sum,i;
+	printf("Enter the value of n");
+	scanf("%d",&n);
+	for(i=0;i<=n;i++)
+	{
+	    sum=sum+i;
+    }
+	printf("%d\n",sum);
+	
+}
